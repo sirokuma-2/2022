52ebdca3a476323fe99327c0c946f2a6dc2efb25
sirokuma-2 <lord_ring_2006@icloud.com>
2023-01-04 21:05:09 +0900

    property_analysis

--- property_analysis/property_analysis.py
+++ property_analysis/property_analysis.py
@@ -2,6 +2,7 @@
 import datetime
 import pandas as pd 
 import numpy as np
+import xgboost as xgb
 
 # MSE
 from sklearn.metrics import mean_squared_error as MSE
@@ -9,15 +10,37 @@ from sklearn.metrics import mean_squared_error as MSE
 #本日の日付
 dt_now = datetime.datetime.now().strftime('%Y%m%d')
 
+dt_now = '20230101'
+
+drop_columns=['Unnamed: 0','city','lng','lat']
 
 df = pd.read_csv(dt_now+'data_mod6.csv')
-df = df.drop(columns=['Unnamed: 0','city','lng','lat'])
+df = df.drop(columns=drop_columns)
+df = df.drop_duplicates()
 
 #異常値の除外
-df = df.query('price < 80000000')
+df = df.query('price > 15000000' and 'price < 25000000')
+
+#利回りの置換
+def rate_mean(x):
+    if x.structure == 0:
+        x= 7.70
+    elif x.structure == 1:
+        x=7.50
+    elif x.structure == 2:
+        x=8.24
+    elif x.structure == 3:
+        x=7.12
+        
+    return x
+    
+df['rate'] = df.apply(lambda x:rate_mean(x),axis=1)
 
 #不要列の除外
-df_t = df.drop(['address','structure','price'], axis=1)
+df_t = df.drop(['address','structure','price',], axis=1)
+df_t = df_t.reset_index()
+df_copy = df_t
+
 
 #標準化
 from sklearn.preprocessing import StandardScaler
@@ -28,6 +51,7 @@ df_t = scaler.transform(df_t)
 # 説明変数をdata_Xに、目的変数をdata_yに代入
 data_X =df_t
 data_y = df['price']
+
 
 #train_test_splitのインポート
 from sklearn.model_selection import train_test_split
@@ -46,12 +70,13 @@ from sklearn.ensemble import RandomForestRegressor
 
 # モデルの箱を代入する変数名をrfとし、モデルを表す箱を準備しましょう。
 rf= RandomForestRegressor(random_state=42)
+xgb= xgb.XGBRegressor()
 
 # 学習データの説明変数を表す変数X_trainと、目的変数を表す変数y_trainを用いて、モデルの学習を行いましょう。
-rf.fit(X_train, y_train)
+xgb.fit(X_train, y_train)
 
 # 評価データに対する予測を行い、その結果を変数predに代入してください。
-pred = rf.predict(X_test)
+pred = xgb.predict(X_test)
 
 # 予測精度の確認 RMSE
 print(round(np.sqrt(MSE(y_test, pred))))
@@ -72,22 +97,37 @@ result['distinct'] = result['test'] - result['pred']
 result['distinct_rate'] = (result['distinct']/result['test'])*100
 result['distinct_rate_abs'] = ((result['distinct']/result['test'])*100).abs()
 
+
+
 #インデックスのリセット
 result = result.set_index('address_ID')
 
+df = pd.read_csv(dt_now+'data_mod6.csv')
+
+df = df.drop(columns=['Unnamed: 0'])
+df = df.rename(columns={'lng': 'Latitude','lat':'Longitude'})
+
 #元々のデータフレームに統合
 result = pd.merge(df,result, how='right', left_index=True, right_index=True)
-#print(result.head())
+result['rsme'] = abs((result['pred']-result['price'])**2)
 
+#print(result.head())
 #テストデータのIDの整理
 test_ID = result.index
 print(test_ID)
 
 #訓練データのエクスポート
+
+
 df = pd.read_csv(dt_now+'data_mod6.csv')
-df = df.drop(columns=['Unnamed: 0','city','lng','lat'])
-train = df.drop(df.index[test_ID])
+train = df.drop(columns=['Unnamed: 0'])
+train = train.drop_duplicates()
+train = train.query('price < 80000000')
+train = train.drop(df.index[test_ID])
+print(train.shape[0])
+
 train.to_csv('train.csv', header=True, index=False)
+
 
 #テストデータのエクスポート
 result.to_csv('result.csv', header=True, index=False)
